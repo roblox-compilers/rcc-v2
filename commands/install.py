4feace8c6a248b540baabb8904498946b4f4bb97
Bulldo344 <policelaw14@gmail.com>
2024-04-14 16:12:37 -0400

    added install options and documented some of the todos

--- commands/install.py
+++ commands/install.py
@@ -1,10 +1,38 @@
-import click 
+import click, inquirer, os 
+
+from inquirer import List
+
+def display_option_menu(options, msg):
+    questions = [
+        List('option',
+             message=msg,
+             choices=options)
+    ]
+
+    answers = inquirer.prompt(questions)
+    selected_option = answers['option']
+    return selected_option
 
 class Install:
     def __init__(self, cli: click.Group):
-        @cli.command()
-        @click.argument('package_name')
-        def install(package_name):
-            print(f"Installing {package_name}")
+        @click.command()
+        @click.option('--build', default=False, is_flag=True)
+        @click.option('--demo', default=False, is_flag=True)
+        def install(build, demo):            
+            options = ["roblox-py", "fullstack", "roblox-c (C)", "roblox-c (C++)", "roblox-cs"]
+
+            chosen = display_option_menu(options, "Select a compiler")
+            print(chosen)
             
+            if build == True:
+                # TODO: pulls directly from the github releases, and installs the .exe file
+                print("Installing latest version...")
+            
+            if demo == True:
+                # TODO: install the templates folder to their folder (whatever they cd into)
+                # TODO: change the ending of the files to match their option, so roblox-py is .py etc etc
+                # TODO: ^, make sure you edit the files after you change their ending to the respected print statement for the lang
+                print("Installing demo...")
+            
+        cli.add_command(install)
         cli()
--- rcc.py
+++ rcc.py
@@ -6,6 +6,29 @@ from commands.install import Install
 def group():
     pass
 
+@click.command()
+def rcc():
+    print("usage: \033[31;1mrcc\033[0m [command] [options]")
+    print("\033[1mCommands:\033[0m")
+    print("  \033[1mhelp\033[0m\t\topen this help menu")
+    print("  \033[1minit\033[0m\t\tinitialize a project")
+    print("  \033[1minstall\033[0m\tinstall a compiler")
+    print("  \033[1muninstall\033[0m\tuninstall a compiler")
+    print("  \033[1mupdate\033[0m\tupdate a compiler")
+    print("\033[1mOptions:\033[0m")
+    print("  \033[1m-o\033[0m\t\toutput directory (default: out)")
+    print("  \033[1m<none>\033[0m\tinput directory (default: src)")
+    print("  \033[1m-d\033[0m\t\tdebug mode")
+    print("\033[1mExamples:\033[0m")
+    print("  \033[1mrcc\033[0m \033[1minstall\033[0m roblox-py")
+    print("  \033[1mrcc\033[0m \033[1muninstall\033[0m roblox-py")
+    print("  \033[1mrcc\033[0m \033[1mupdate\033[0m roblox-py")
+    print("  \033[1mrcc\033[0m \033[1m-o\033[0m out")
+    print("  \033[1mrcc\033[0m \033[1m-o\033[0m out src")
+    print("  \033[1mrcc\033[0m \033[1minclude\033[0m @roblox/roact")
+
+
 
 if __name__ == '__main__':
+    group.add_command(rcc)
     Install(group)
